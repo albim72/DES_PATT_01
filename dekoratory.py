358f92acb92de29a9776187180d6baadaef4d1e4
albim72 <marcin.albiniak72@gmail.com>
2024-11-09 11:29:52 +0100

    Update dekoratory.py

--- dekoratory.py
+++ dekoratory.py
@@ -85,3 +85,29 @@ try:
     print(mnozenie(6,"osiem"))
 except TypeError as tp:
     print(tp)
+
+#przykład 5 - dekorato służący do memizacji wyników
+
+def memoizacja(funkcja):
+    cache = {}
+
+    def wrapper(*args):
+        if args in cache:
+            print(f"Zwracanie wyniku z cache dla argumentow: {args}")
+            print(f"funkcja: {funkcja.__name__}({args}->{funkcja(*args)})")
+            return cache[args]
+        else:
+            wynik=funkcja(*args)
+            cache[args] = wynik
+            return wynik
+    return wrapper
+
+
+@memoizacja
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(10))
