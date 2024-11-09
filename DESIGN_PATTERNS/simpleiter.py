3e892a169b730e08289eb5734589608a422f1bec
albim72 <marcin.albiniak72@gmail.com>
2024-11-09 16:26:34 +0100

    Create simpleiter.py

---
+++ DESIGN_PATTERNS/simpleiter.py
@@ -0,0 +1,13 @@
+nb = [34,25,75,84,94,93,24,2,6]
+
+iterator = iter(nb)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
