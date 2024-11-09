0814b3acca34282f06b262b837274a89c647bde0
albim72 <marcin.albiniak72@gmail.com>
2024-11-09 15:34:00 +0100

    Update main.py

--- testowanie_meta/main.py
+++ testowanie_meta/main.py
@@ -1,6 +1,15 @@
 import pytest
 from metainfo import MojaKlasa
+from enforce import ValidClass, EnforceAttributeMeta
 
 def test_meta_info_added():
     assert hasattr(MojaKlasa,'meta_info'),"Klasa nie ma atrybutu 'meta_info'"
     assert MojaKlasa.meta_info == "Dodano przez metaklasę","Atrybut 'meta_info' posiada niewłaściwą zawartość"
+
+def test_valid_class():
+    assert hasattr(ValidClass,'required_attribute'),"Klasa ValidClass nie posiada atrybytu - required_attribute"
+
+def test_invalid_class():
+    with pytest.raises(TypeError,match="Klasa musi mieć atrybut - 'required_attribute'"):
+        class InvalidClass(metaclass=EnforceAttributeMeta):
+            pass
